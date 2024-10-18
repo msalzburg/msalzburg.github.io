62b2618141dc5b5247d4803dbda2cddf7c876ac3
msalzburg <salzburg@emjot.de>
2024-10-18 15:23:21 +0200

    Use vitest coverage "instanbul" provider

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,7 +5,8 @@ export default getViteConfig({
   test: {
     // Vitest configuration options
     coverage: {
-      exclude: ["astro.config.mjs", "vitest.config.ts", "tailwind.config.cjs", "src/env.d.ts"]
+      exclude: ["astro.config.mjs", "vitest.config.ts", "tailwind.config.cjs", "src/env.d.ts"],
+      provider: "istanbul"
     }      
   },
 });
